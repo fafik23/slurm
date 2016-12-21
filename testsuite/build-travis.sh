f49a379aed40c6e3000187f8df8a6cf5fd604431
Dominik Bartkiewicz <bart@schedmd.com>
2016-12-21 12:58:26 +0100

    test

--- testsuite/build-travis.sh
+++ testsuite/build-travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$SLURMD_VER" != "$TRAVIS_BRANCH"]] && [$MIGRATION -eq 1]; then
+if [[ "$SLURMD_VER" != "$TRAVIS_BRANCH" ]] && [ $MIGRATION -eq 1 ] ; then
 	git checkout $SLURMD_VER
 	echo "Compile $SLURM for Slurmd"
 	./configure --enable-multiple-slurmd --prefix=/tmp/slurm_d/ > /dev/null
@@ -11,7 +11,7 @@ else
 	ln -s /tmp/slurm /tmp/slurm_d
 fi
 
-if [[ "$SLURMCTL_VER" != "$TRAVIS_BRANCH"]] && [$MIGRATION -eq 1]; then
+if [[ "$SLURMCTL_VER" != "$TRAVIS_BRANCH" ]] && [ $MIGRATION -eq 1 ] ; then
 	git checkout $SLURMCTL_VER
 	echo "Compile $SLURMCTL_VER for SlurmCTL"
 	./configure --enable-multiple-slurmd --prefix=/tmp/slurm_ctl/ > /dev/null
@@ -22,7 +22,7 @@ else
 	ln -s /tmp/slurm /tmp/slurm_ctl
 fi
 
-if [[ "$SLURMCLI_VER" != "$TRAVIS_BRANCH"]] && [$MIGRATION -eq 1] ; then
+if [[ "$SLURMCLI_VER" != "$TRAVIS_BRANCH" ]] && [ $MIGRATION -eq 1 ] ; then
 	echo "Compile $SLURMCLI_VER for Slurm CLI"
 	git checkout $SLURMCLI_VER
 	./configure --enable-multiple-slurmd --prefix=/tmp/slurm_cli/ > /dev/null
@@ -33,7 +33,7 @@ else
 	ln -s /tmp/slurm /tmp/slurm_cli
 fi
 
-if [[ "$SLURMDB_VER" != "$TRAVIS_BRANCH"]] && [$MIGRATION -eq 1]; then
+if [[ "$SLURMDB_VER" != "$TRAVIS_BRANCH" ]] && [ $MIGRATION -eq 1 ] ; then
 	echo "Compile $SLURMDB_VER for SlurmDB"
 	git checkout $SLURMDB_VER
 	./configure --enable-multiple-slurmd --prefix=/tmp/slurm_db/ > /dev/null
